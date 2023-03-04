0dfa10fbb0c12ca4ad9aafa1a39fde47e2a1caa8
vishnupullakhandam <104721847+vishnupullakhandam@users.noreply.github.com>
2023-03-04 16:33:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Everybody! This is VTU17716'));
+app.get('/', (req, res) => res.send('Hello Everybody! This is VTU18002'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
